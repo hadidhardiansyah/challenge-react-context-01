d33c3b423548d4d019e72b861e5128d1b52663ae
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2023-01-07 05:00:47 +0700

    Challenge React Context User Info

--- src/context/UserInfo.js
+++ src/context/UserInfo.js
@@ -5,7 +5,6 @@ export const UserInfoContext = React.createContext(null);
 export const UserInfoProvider =({children}) =>{
     const [firstName, setFirstName] = React.useState("");
     const [lastName, setLastName] = React.useState("");
-
     const onChangeFirstName = (e) => setFirstName(e.target.value);
     const onChangeLastName = (e) => setLastName(e.target.value);
 
